1bc7f4a0d0d5f8faf66de52f80e8f6a63455df7a
cehirim1 <Humbled1st@gmail.com>
2023-01-21 19:41:56 -0800

    added files to repository

---
+++ src/main/java/edu/greenriver/sdev/random_num_generator/RandomNumGeneratorApplication.java
@@ -0,0 +1,13 @@
+package edu.greenriver.sdev.random_num_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomNumGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomNumGeneratorApplication.class, args);
+    }
+
+}
